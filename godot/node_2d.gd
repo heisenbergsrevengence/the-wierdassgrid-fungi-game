bc4d0c99af952b36e7cf61fdfe268c8cb5d96a99
heisenbergsrevengence <146832254+heisenbergsrevengence@users.noreply.github.com>
2024-01-23 22:20:02 +0530

    Delete useless godot dir

--- godot/node_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-#this code is for the title screen, it opens the gm file and also redirects player to title screen
-func _ready():
-	DisplayServer.window_set_title("The bourgeoisie")
-	get_tree().change_scene_to_file("res://path/to/main_menu.tscn")
-	await get_tree().create_timer(5).timeout # Display logo for 5 seconds
-	get_node("Lgog").queue_free() # Delete the logo node then tell tileHolder to add its children to the scene
-	get_node("TileHolder").addChildren()
-	pass
